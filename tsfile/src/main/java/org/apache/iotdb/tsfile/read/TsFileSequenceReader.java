d61fca06f30dd7fe86783c24d6750e9e1c0470c7
JackieTien97 <JackieTien@foxmail.com>
2020-11-12 11:13:14 +0800

    fix nullpointer

--- tsfile/src/main/java/org/apache/iotdb/tsfile/read/TsFileSequenceReader.java
+++ tsfile/src/main/java/org/apache/iotdb/tsfile/read/TsFileSequenceReader.java
@@ -956,6 +956,7 @@ public class TsFileSequenceReader implements AutoCloseable {
    */
   public List<ChunkMetadata> readChunkMetaDataList(TimeseriesMetadata timeseriesMetaData)
       throws IOException {
+    readFileMetadata();
     List<Pair<Long, Long>> versionInfo = tsFileMetaData.getVersionInfo();
     ArrayList<ChunkMetadata> chunkMetadataList = new ArrayList<>();
     long startOffsetOfChunkMetadataList = timeseriesMetaData.getOffsetOfChunkMetaDataList();
